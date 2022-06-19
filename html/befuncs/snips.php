542fffbdb8ea6e7ed8784737013134fdbed492da
RiedleroD <riedler@gelse.eu>
2022-06-19 16:28:08 +0200

    initial C

---
+++ html/befuncs/snips.php
@@ -0,0 +1,53 @@
+<?php
+	function genUsual($title,$style,$custom){
+		echo
+		'<!DOCTYPE html>'.
+		'<html lang="en">'.
+			'<head>'.
+				'<meta charset="UTF-8"/>'.
+				'<meta name="viewport" content="width=device-width, initial-scale=1.0">'.
+				"<title>$title</title>".
+				'<link rel="icon" type="image/svg" href="/favicon.svg"/>'.
+				'<style>'.
+					'@import "/style/default.css";'.
+					$style.
+				'</style>'.
+				$custom.
+			'</head>';
+	}
+	function genNavBar($items){
+		echo '<nav id="main_nav"><img id="logo" src="https://riedler.wien/favicon.svg"/>';
+		foreach($items as $txt=>$lnk){
+			echo "<a href='$lnk' class='btn'>$txt</a>";
+		}
+		echo '</nav>';
+	}
+	#HeBi → Heterogenous & Bilateral
+	#meaning it's two-sided with different content in each side.
+	#in this case it's an icon left and some text right
+	function genHeBiLink($img,$txt,$href){
+		if($img==null){
+			$img='<div></div>';
+		}else{
+			$img="<img src='$img'/>";
+		}
+		echo " <a href='$href' class='btn hebi'>$img$txt</a> ";
+	}
+	function genHeBi($img,$txt){
+		if($img==null){
+			$img='<div></div>';
+		}else{
+			$img="<img src='$img'/>";
+		}
+		echo " <span class='btn hebi'>$img$txt</span> ";
+	}
+	#no idea what sfto means, but it's been the folder with all my images for
+	#quite some time, so it's staying that way.
+	#TODO: replace with databse function
+	function sfto($name){
+		return "https://riedler.wien/sfto/$name";
+	}
+	function rwicon($name){
+		return "https://riedler.wien/sfto/rwicons/$name.svg";
+	}
+?>
---
+++ html/index.php
@@ -0,0 +1,84 @@
+<?php
+	include("befuncs/snips.php");
+	genUsual("Riedler","","");
+?>
+<body>
+	<?php 
+		genNavBar(array("Coding"=>"https://riedler.wien/coding/","Music"=>"https://riedler.wien/music/","Home"=>"./index.php"));
+	?>
+	<div class="slant">
+		<h1>Riedler</h1>
+		<h3 style="text-align:center">External Links</h3>
+		<fieldset>
+			<legend><h3>Support me!</h3></legend>
+			<?php
+				#TODO: custom icon for liberapay and fiverr (this is ugleh)
+				genHeBiLink(null,"Liberapay","https://liberapay.com/Riedler");
+				genHeBiLink(rwicon("pt"),"Patreon","https://patreon.com/RiedlerM");
+				genHeBiLink(null,"Fiverr","https://www.fiverr.com/s2/1e37f97239");
+			?>
+		</fieldset>
+		<fieldset>
+			<legend><h3>Music</h3></legend>
+			<?php
+				genHeBiLink(rwicon("yt"),"Youtube","https://www.youtube.com/channel/UC0aIZx6FIHB5Fq_sr0TMXSw");
+				genHeBiLink(rwicon("lmms"),"LMMS Sharing Platform","https://lmms.io/lsp/?action=browse&user=Riedler");
+				genHeBiLink(rwicon("lbry"),"LBRY","lbry://@Riedler:6");
+				genHeBiLink(rwicon("oy"),"Odysee","https://odysee.com/@Riedler:6");
+				genHeBiLink(rwicon("npstr"),"Napster","https://at.napster.com/artist/riedler");
+				genHeBiLink(rwicon("bl"),"BandLab","https://bandlab.com/riedler");
+				genHeBiLink(rwicon("sy"),"Spotify","https://open.spotify.com/artist/7k9sRjqYP68ZI8Bw8BwmuG");
+				genHeBiLink(rwicon("ht"),"Hearthis","https://hearthis.at/riedler/");
+				genHeBiLink(rwicon("bp"),"Boomplay","https://www.boomplay.com/artists/19145926");
+				genHeBiLink(rwicon("az"),"Amazon","https://www.amazon.com/s?k=Riedler&i=digital-music&search-type=ss");
+				genHeBiLink(rwicon("am"),"Amazon Music","https://music.amazon.com/artists/B08QG41MYN/riedler");
+				genHeBiLink(rwicon("ih"),"iHeart","https://www.iheart.com/artist/riedler-36682916/");
+				genHeBiLink(rwicon("sc"),"SoundCloud","https://soundcloud.com/riedler-music");
+				genHeBiLink(rwicon("apm"),"Apple Music","https://music.apple.com/gb/artist/riedler/1544612571");
+				genHeBiLink(rwicon("yx"),"Yandex Music","https://music.yandex.com/artist/10521437");
+				genHeBiLink(rwicon("sbzv"),"СберЗвук","https://sber-zvuk.com/artist/157131");
+				genHeBiLink(rwicon("dz"),"Deezer","https://www.deezer.com/en/artist/116666602");
+				genHeBiLink(rwicon("sz"),"Shazam","https://www.shazam.com/gb/artist/211983431");
+				genHeBiLink(rwicon("vimeo"),"Vimeo","https://vimeo.com/user125791194");
+				genHeBiLink(rwicon("td"),"Tidal","https://tidal.com/browse/artist/22622017");
+				genHeBiLink(rwicon("ne"),"NetEase","https://music.163.com/#/artist?id=46345905");
+				genHeBiLink(rwicon("qq"),"QQ","https://y.qq.com/n/ryqq/singer/0031Xw4m1zLAUa");
+				genHeBiLink(rwicon("vn"),"Vibe/Naver","https://vibe.naver.com/artist/4810478");
+			?>
+		</fieldset>
+		<fieldset>
+			<legend><h3>Coding</h3></legend>
+			<?php
+				genHeBiLink(null,"Github","https://github.com/RiedleroD/");
+				genHeBiLink(null,"GitLab","https://gitlab.com/RiedleroD");
+				genHeBiLink(null,"CodeBerg","https://codeberg.org/Riedler");
+				genHeBiLink(rwicon("yt"),"YouTube","https://www.youtube.com/channel/UCZQenB17Kia40e0Blx_xsTQ")
+			?>
+		</fieldset>
+		<fieldset>
+			<legend><h3>Userstyles</h3></legend>
+			<?php
+				genHeBiLink(null,"userstyles.world","https://userstyles.world/user/riedler");
+				genHeBiLink(null,"Userstyles Repository","https://github.com/RiedleroD/userstyles-riedler");
+			?>
+		</fieldset>
+		<fieldset>
+			<legend><h3>Social</h3></legend>
+			<?php
+				genHeBiLink(null,"Mastodon","https://mas.to/@Riedler");
+				genHeBiLink(null,"Telegram Channel","https://t.me/RiedlerM");
+				genHeBiLink(null,"Subreddit","https://www.reddit.com/r/Riedler");
+			?>
+		</fieldset>
+		<fieldset>
+			<legend><h3>Gaming</h3></legend>
+			<?php
+				genHeBiLink(null,"speedrun.com","https://www.speedrun.com/user/Riedler");
+				genHeBiLink(null,"Mega Man Leaderboards","https://megamanleaderboards.net/index.php?page=runner&name=Riedler");
+				genHeBiLink(rwicon("yt"),"Youtube","https://www.youtube.com/channel/UCCvjTX0Sy29dkMSZ5V8rM5Q")
+			?>
+		</fieldset>
+	</div>
+	<div id="overlay"></div>
+</body>
+</html>
