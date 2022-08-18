6fbaf3db7b0052a4c5c7678bd444ccbef5e1ff36
RiedleroD <riedler@gelse.eu>
2022-08-18 14:55:50 +0200

    also search for parent anchors on click wrapper & fix animation not returning to default when the user goes back a page

--- html/jizz/default.js
+++ html/jizz/default.js
@@ -1,17 +1,30 @@
 document.addEventListener("click",function(event){
 	event.preventDefault();
-	if(event.target.tagName=='A' && event.target.href){
-		//preload next website while animation plays out
-		//(not working, but what the hell. I did it like in the MDN example. Not my fault.)
-		let preload=document.createElement("link");
-		preload.href=event.target.href;
-		preload.rel="preload";
-		preload.as="document";
-		document.head.appendChild(preload);
-		//reversing load-in animation
-		let overlay = document.getElementById("overlay");
-		overlay.getAnimations()[0].reverse();
-		//wait 500ms for animation to finish and then redirect
-		setTimeout(()=>{window.location.href=event.target.href},500);
+	let anchor = event.target
+	while(anchor){
+		if(anchor.tagName=='A' && anchor.href){
+			//preload next website while animation plays out
+			//(not working, but what the hell. I did it like in the MDN example. Not my fault.)
+			let preload=document.createElement("link");
+			preload.href=anchor.href;
+			preload.rel="preload";
+			preload.as="document";
+			document.head.appendChild(preload);
+			//reversing load-in animation
+			let overlay = document.getElementById("overlay");
+			overlay.getAnimations()[0].reverse();
+			//wait 500ms for animation to finish and then redirect
+			setTimeout(()=>{window.location.href=anchor.href},500);
+			return;
+		}
+		anchor=anchor.parentElement;//checking if parent contains a link
 	}
 },false);
+
+window.addEventListener("focus",function(event){
+	let overlay = document.getElementById("overlay");
+	let anim=overlay.getAnimations()[0];
+	if(anim.currentTime==0 && anim.playState=="finished"){
+		anim.reverse();
+	}
+});
