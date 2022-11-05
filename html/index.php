f7cea4cd1bb1efd792b867319d32d0d62ddb794a
RiedleroD <riedler@gelse.eu>
2022-11-05 14:33:04 +0100

    main page now uses DB for some links

--- html/index.php
+++ html/index.php
@@ -1,5 +1,7 @@
 <?php
-	include("befuncs/snips.php");
+	require_once("befuncs/snips.php");
+	require_once("befuncs/db_music.php");
+	require_once("befuncs/db_coding.php");
 	genUsual("Riedler","@import 'style/home.css'","");
 ?>
 <body>
@@ -17,44 +19,21 @@
 		<fieldset>
 			<legend><h3>Music</h3></legend>
 			<?php
-				genHeBiLink(rwicon("yt"),"Youtube","https://www.youtube.com/channel/UC0aIZx6FIHB5Fq_sr0TMXSw");
-				genHeBiLink(rwicon("lmms"),"LMMS Sharing Platform","https://lmms.io/lsp/?action=browse&user=Riedler");
-				genHeBiLink(rwicon("lbry"),"LBRY","lbry://@Riedler:6");
-				genHeBiLink(rwicon("oy"),"Odysee","https://odysee.com/@Riedler:6");
-				genHeBiLink(rwicon("npstr"),"Napster","https://at.napster.com/artist/riedler");
-				genHeBiLink(rwicon("bl"),"BandLab","https://bandlab.com/riedler");
-				genHeBiLink(rwicon("sy"),"Spotify","https://open.spotify.com/artist/7k9sRjqYP68ZI8Bw8BwmuG");
-				genHeBiLink(rwicon("ht"),"Hearthis","https://hearthis.at/riedler/");
-				genHeBiLink(rwicon("bp"),"Boomplay","https://www.boomplay.com/artists/19145926");
-				genHeBiLink(rwicon("az"),"Amazon","https://www.amazon.com/s?k=Riedler&i=digital-music&search-type=ss");
-				genHeBiLink(rwicon("am"),"Amazon Music","https://music.amazon.com/artists/B08QG41MYN/riedler");
-				genHeBiLink(rwicon("ih"),"iHeart","https://www.iheart.com/artist/riedler-36682916/");
-				genHeBiLink(rwicon("sc"),"SoundCloud","https://soundcloud.com/riedler-music");
-				genHeBiLink(rwicon("apm"),"Apple Music","https://music.apple.com/gb/artist/riedler/1544612571");
-				genHeBiLink(rwicon("yx"),"Yandex Music","https://music.yandex.com/artist/10521437");
-				genHeBiLink(rwicon("sbzv"),"СберЗвук","https://sber-zvuk.com/artist/157131");
-				genHeBiLink(rwicon("dz"),"Deezer","https://www.deezer.com/en/artist/116666602");
-				genHeBiLink(rwicon("sz"),"Shazam","https://www.shazam.com/gb/artist/211983431");
-				genHeBiLink(rwicon("vm"),"Vimeo","https://vimeo.com/user125791194");
-				genHeBiLink(rwicon("td"),"Tidal","https://tidal.com/browse/artist/22622017");
-				genHeBiLink(rwicon("ne"),"NetEase","https://music.163.com/#/artist?id=46345905");
-				genHeBiLink(rwicon("qq"),"QQ","https://y.qq.com/n/ryqq/singer/0031Xw4m1zLAUa");
-				genHeBiLink(rwicon("vn"),"Vibe/Naver","https://vibe.naver.com/artist/4810478");
+				$db=new musicdb();
+				services_as_html($db->get_services());
 			?>
 		</fieldset>
 		<fieldset>
 			<legend><h3>Coding</h3></legend>
 			<?php
-				genHeBiLink(rwicon("gh"),"Github","https://github.com/RiedleroD/");
-				genHeBiLink(rwicon("gl"),"GitLab","https://gitlab.com/RiedleroD");
-				genHeBiLink(rwicon("cb"),"CodeBerg","https://codeberg.org/Riedler");
-				genHeBiLink(rwicon("yt"),"YouTube","https://www.youtube.com/channel/UCZQenB17Kia40e0Blx_xsTQ")
+				$db=new codingdb();
+				services_as_html($db->get_services());
 			?>
 		</fieldset>
 		<fieldset>
 			<legend><h3>Userstyles</h3></legend>
 			<?php
-				genHeBiLink(rwicon("usw"),"userstyles.world","https://userstyles.world/user/riedler");
+				genHeBiLink(rwicon("usw"),"Userstyles.world","https://userstyles.world/user/riedler");
 				genHeBiLink(rwicon("gh"),"Userstyles Repository","https://github.com/RiedleroD/userstyles-riedler");
 			?>
 		</fieldset>
