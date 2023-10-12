6cb1cc8b2b461ae3e49900ae452f62fcec66c50b
RiedleroD <github@riedler.wien>
2023-10-12 23:44:13 +0200

    updated link collection on the landing page

--- html/index.php
+++ html/index.php
@@ -8,12 +8,10 @@
 	<?php genNavBar(); ?>
 	<div class="slant">
 		<h1>Riedler</h1>
-		<h3 style="text-align:center">External Links</h3>
 		<fieldset>
 			<legend><h3>Support me!</h3></legend>
 			<?php
 				genHeBiLink(rwicon("lp"),"Liberapay","https://liberapay.com/Riedler");
-				genHeBiLink(rwicon("fv"),"Fiverr","https://www.fiverr.com/s2/1e37f97239");
 				genHeBiLink(rwicon("kf"),"Ko-fi","https://ko-fi.com/riedler")
 			?>
 		</fieldset>
@@ -32,18 +30,10 @@
 			?>
 		</fieldset>
 		<fieldset>
-			<legend><h3>Userstyles</h3></legend>
-			<?php
-				genHeBiLink(rwicon("usw"),"Userstyles.world","https://userstyles.world/user/riedler");
-				genHeBiLink(rwicon("gh"),"Userstyles Repository","https://github.com/RiedleroD/userstyles-riedler");
-			?>
-		</fieldset>
-		<fieldset>
 			<legend><h3>Social</h3></legend>
 			<?php
-				genHeBiLink(rwicon("mstdn"),"Mastodon","https://mas.to/@Riedler");
-				genHeBiLink(rwicon("tg"),"Telegram Channel","https://t.me/RiedlerM");
-				genHeBiLink(rwicon("rddt"),"Subreddit","https://www.reddit.com/r/Riedler");
+				genHeBiLink(rwicon("mstdn"),"Mastodon","https://catcatnya.com/@Riedler");
+				genHeBiLink(rwicon("tg"),"Telegram","https://t.me/@RiedleroD");
 			?>
 		</fieldset>
 		<fieldset>
