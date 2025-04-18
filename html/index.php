a313a7080fbc21a9b6a8eafcbe67b896877f07dd
Riedler <github@riedler.wien>
2025-04-18 09:10:46 +0200

    basic new layout for main page

--- html/index.php
+++ html/index.php
@@ -5,39 +5,12 @@
 	genUsual("Riedler",['/style/home.css'],"");
 ?>
 <body>
-	<?php genNavBar(); ?>
-	<div class="slant">
+	<main>
 		<h1>Riedler</h1>
-		<fieldset>
-			<legend><h3>Support me!</h3></legend>
-			<?php
-				genHeBiLink(rwicon("lp"),"Liberapay","https://liberapay.com/Riedler");
-				genHeBiLink(rwicon("kf"),"Ko-fi","https://ko-fi.com/riedler");
-				genHeBiLink(rwicon("gh"),"Github","https://github.com/sponsors/RiedleroD");
-				genHeBiLink(rwicon("pt"),"Patreon","https://www.patreon.com/Riedler");
-			?>
-		</fieldset>
-		<fieldset>
-			<legend><h3>Main Navigation</h3></legend>
-			<p class="unslant" style="margin-left:1em">See in the top right! The website will be redesigned later to make this less bad &gt;.&lt;</p>
-		</fieldset>
-		<fieldset>
-			<legend><h3>Social</h3></legend>
-			<?php
-				genHeBiLink(rwicon("akko"),"Akkoma","https://donotsta.re/_r");
-				genHeBiLink(rwicon("mtrx"),"Matrix","https://matrix.to/#/@riedler:masfloss.net");
-			?>
-		</fieldset>
-		<fieldset>
-			<legend><h3>Gaming</h3></legend>
-			<?php
-				genHeBiLink(rwicon("mnspr"),"Minesweeper","https://minesweeper.online/player/7449348");
-				genHeBiLink(rwicon("spdrn"),"speedrun.com","https://www.speedrun.com/user/Riedler");
-				genHeBiLink(rwicon("mmlb"),"Mega Man Leaderboards","https://megamanleaderboards.net/index.php?page=runner&name=Riedler");
-				genHeBiLink(rwicon("yt"),"Youtube","https://www.youtube.com/channel/UCCvjTX0Sy29dkMSZ5V8rM5Q")
-			?>
-		</fieldset>
-	</div>
+		<a href="music">Music</a>
+		<a href="coding">Code</a>
+		<a href="about">About</a>
+	</main>
 	<?php genFooter(); ?>
 </body>
 </html>
